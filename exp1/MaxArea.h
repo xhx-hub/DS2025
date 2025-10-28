5dd23b8ef4a4a0b206fb9baba70aa54edaf2f5d5
xhx-hub <3439213477@qq.com>
2025-10-28 23:48:34 +0800

    Add MaxArea class header for rectangle area calculation

---
+++ exp1/MaxArea.h
@@ -0,0 +1,12 @@
+#ifndef MAXAREA_H
+#define MAXAREA_H
+
+#include <vector>
+using namespace std;
+
+class MaxArea {
+public:
+    int largestRectangleArea(vector<int>& heights);
+};
+
+#endif // MAXAREA_H
